d116181b6f5d36804b92742c191f716ca0d4e55f
Ming Cheng <mcheng991129@gmail.com>
2019-12-21 19:23:53 +0800

    Create solution1.cpp

---
+++ 287. Find the Duplicate Number/solution1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        for (i = 0; i < nums.size() - 1; i ++) {
+            if (nums[i] == nums[i + 1])
+                break;
+        }
+        return nums[i];
+    }
+};
